8a4cc4333fc11d885a75eab3d52758c133fcc3e0
Mjb141 <mike141@live.com>
2023-11-21 16:07:10 +0000

    Session token can be None when not in a role

--- cdk/src/main.py
+++ cdk/src/main.py
@@ -24,7 +24,7 @@ class CdkMod:
         self,
         aws_access_key_id: dagger.Secret,
         aws_secret_access_key: dagger.Secret,
-        aws_session_token: dagger.Secret,
+        aws_session_token: dagger.Secret | None = None,
     ) -> "CdkMod":
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
