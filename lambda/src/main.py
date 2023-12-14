291ddfafb5a037c75f82b19477a9832c690ef569
Mjb141 <mike141@live.com>
2023-12-14 14:31:31 +0000

    Debugged package, now zip it

--- lambda/src/main.py
+++ lambda/src/main.py
@@ -70,9 +70,8 @@ class LambdaMod:
         )
 
         return (
-            container.with_exec(INSTALL_DEPENDENCIES, skip_entrypoint=True).with_exec(
-                INSTALL_PACKAGE, skip_entrypoint=True
-            )
-            # .with_workdir("/dist/lambda/")
-            # .with_exec(ZIP_PACKAGE, skip_entrypoint=True)
+            container.with_exec(INSTALL_DEPENDENCIES, skip_entrypoint=True)
+            .with_exec(INSTALL_PACKAGE, skip_entrypoint=True)
+            .with_workdir("/dist/lambda/")
+            .with_exec(ZIP_PACKAGE, skip_entrypoint=True)
         )
