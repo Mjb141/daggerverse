19eee1ac70168e592905ee5b2bbe109a220b65db
Mjb141 <mike141@live.com>
2023-12-14 14:27:36 +0000

    And skip entrypoint

--- lambda/src/main.py
+++ lambda/src/main.py
@@ -70,7 +70,7 @@ class LambdaMod:
         )
 
         return (
-            container.with_exec(INSTALL_DEPENDENCIES).with_exec(
+            container.with_exec(INSTALL_DEPENDENCIES, skip_entrypoint=True).with_exec(
                 INSTALL_PACKAGE, skip_entrypoint=True
             )
             # .with_workdir("/dist/lambda/")
