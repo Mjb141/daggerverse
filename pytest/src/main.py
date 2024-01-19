d85c85314ee1a6ff22d03557dc0103e46a3de0c4
Mjb141 <mike141@live.com>
2024-01-19 13:32:54 +0000

    Update to 0.9.6

--- pytest/src/main.py
+++ pytest/src/main.py
@@ -1,5 +1,5 @@
 import dagger
-from dagger.mod import function, object_type
+from dagger import dag, function, object_type
 
 BASE_PIP_COMMAND: list[str] = ["pip3", "install", "-r"]
 
@@ -24,7 +24,7 @@ class PytestMod:
 
     def container(self) -> dagger.Container:
         return (
-            dagger.container()
+            dag.container()
             .from_("mikebrown008/cgr-poetry:0.1.4")
             .with_workdir("/src")
             .with_user("root")
