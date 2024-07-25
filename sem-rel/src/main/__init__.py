0c66fd8a69eed075de276291822f56d97894a63a
Mjb141 <46346590+Mjb141@users.noreply.github.com>
2024-07-25 22:48:17 +0100

    Add semantic release config file for tests

--- sem-rel/src/main/__init__.py
+++ sem-rel/src/main/__init__.py
@@ -26,18 +26,13 @@ from dagger import dag, function, object_type
 @object_type
 class SemRel:
     @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
+    def release(self, dir: dagger.Directory) -> dagger.Container:
         """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
-
-    @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
+        return (
             dag.container()
-            .from_("alpine:latest")
-            .with_mounted_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
+            .from_("hoppr/semantic-release")
+            .with_directory("/opt/app", dir)
+            .with_workdir("/opt/app")
+            .with_exec(["ls"])
+            .with_exec(["semantic-release"])
         )
