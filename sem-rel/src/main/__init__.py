d33a7ee9459d592ecde463a15b11cb9ba61387a8
Mjb141 <46346590+Mjb141@users.noreply.github.com>
2024-07-26 15:58:18 +0100

    Tidier

--- sem-rel/src/main/__init__.py
+++ sem-rel/src/main/__init__.py
@@ -24,6 +24,13 @@ from dagger import dag, function, object_type
 # available to Dagger.
 
 
+def construct_cmd(check_if_ci: bool, dry_run: bool) -> list[str]:
+    cmd = ["semantic-release"]
+    cmd = cmd + ["--ci"] if check_if_ci else ["--no-ci"]
+    cmd = cmd + ["--dry-run"] if dry_run else [""]
+    return list(map(str.strip, cmd))
+
+
 @object_type
 class SemRel:
     @function
@@ -47,22 +54,20 @@ class SemRel:
 
         if add_branch:
             branch = await ctr.with_exec(["git", "branch", "--show-current"]).stdout()
-
             print(f"Current Branch: {branch}")
 
             rc_file = await dir.file(".releaserc.json").contents()
             print(f"Current '.releaserc.json':({type(rc_file)})\n {rc_file}")
 
             content = json.loads(rc_file)
-
             if "release" in content:
                 content["release"]["branches"] = {"name": branch.strip()}
             elif "branches" in content:
                 content["branches"] = {"name": branch.strip()}
             else:
-                print(f"No top-level key found in 'content': {content}")
-
-            print(f"Updated '.releaserc.json':\n {content}")
+                raise Exception(
+                    "Neither 'release' nor 'branches' found at top-level in '.releaserc.json'"
+                )
 
             dir = dir.without_file(".releaserc.json").with_new_file(
                 ".releaserc.json", json.dumps(content)
@@ -70,20 +75,11 @@ class SemRel:
 
         env_var_key = "GH_TOKEN" if provider == "github" else "GL_TOKEN"
 
-        cmd = ["semantic-release"]
-        if check_if_ci:
-            cmd = cmd + ["--ci"]
-        else:
-            cmd = cmd + ["--no-ci"]
-
-        if dry_run:
-            cmd = cmd + ["--dry-run"]
-
         return (
             dag.container()
             .from_("hoppr/semantic-release")
             .with_secret_variable(env_var_key, token)
             .with_directory("/src", dir)
             .with_workdir("/src")
-            .with_exec(cmd)
+            .with_exec(construct_cmd(check_if_ci, dry_run))
         )
