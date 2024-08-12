4310152a956ce8f25a5e0aa4cea73d1dad482008
Mjb141 <46346590+Mjb141@users.noreply.github.com>
2024-08-12 19:43:50 +0100

    Start moving config to it's own function

--- sem-rel/src/main/__init__.py
+++ sem-rel/src/main/__init__.py
@@ -13,9 +13,10 @@ rest is a long description with more detail on the module's purpose or usage,
 if appropriate. All modules should have a short description.
 """
 
+from typing import Annotated, Dict
 import dagger
 import json
-from dagger import dag, function, object_type
+from dagger import dag, function, object_type, Doc
 
 # NOTE: it's recommended to move your code into other files in this package
 # and keep __init__.py for imports only, according to Python's convention.
@@ -36,6 +37,39 @@ def construct_cmd(check_if_ci: bool, dry_run: bool):
 
 @object_type
 class SemRel:
+    config_file: Annotated[
+        dagger.File | None, Doc("Path to the .releaserc.json file.")
+    ] = None
+    config: str | None = None
+
+    @function
+    async def with_config(
+        self,
+        file: Annotated[
+            dagger.File | None,
+            Doc("[Required] The relative path to a .releaserc.json file."),
+        ] = None,
+        branch: Annotated[
+            str | None,
+            Doc("[Optional] The branch you want to add to the release configuration."),
+        ] = None,
+    ) -> "SemRel":
+        """Modify the Semantic Release config file (.releaserc.json) for testing purposes."""
+        contents = json.loads(await file.contents())
+
+        if branch:
+            if "release" in contents:
+                contents["release"]["branches"] = {"name": branch.strip()}
+            elif "branches" in contents:
+                contents["branches"] = {"name": branch.strip()}
+            else:
+                raise Exception(
+                    "Branch: Neither 'release' nor 'branches' found at top-level in '.releaserc.json'"
+                )
+
+        self.config = contents
+        return self
+
     @function
     async def release(
         self,
@@ -44,39 +78,14 @@ class SemRel:
         token: dagger.Secret,
         check_if_ci: bool = True,
         dry_run: bool = True,
-        add_branch: bool = False,
     ) -> dagger.Container:
         """Returns a container that runs semantic-release on your branch."""
-
-        ctr = (
-            dag.container()
-            .from_("hoppr/semantic-release")
-            .with_directory("/src", dir)
-            .with_workdir("/src")
-        )
-
-        if add_branch:
-            branch = await ctr.with_exec(["git", "branch", "--show-current"]).stdout()
-            print(f"Current Branch: {branch}")
-
-            rc_file = await dir.file(".releaserc.json").contents()
-            print(f"Current '.releaserc.json':({type(rc_file)})\n {rc_file}")
-
-            content = json.loads(rc_file)
-            if "release" in content:
-                content["release"]["branches"] = {"name": branch.strip()}
-            elif "branches" in content:
-                content["branches"] = {"name": branch.strip()}
-            else:
-                raise Exception(
-                    "Neither 'release' nor 'branches' found at top-level in '.releaserc.json'"
-                )
-
-            dir = dir.without_file(".releaserc.json").with_new_file(
-                ".releaserc.json", json.dumps(content)
-            )
-
         env_var_key = "GH_TOKEN" if provider == "github" else "GL_TOKEN"
+
+        if self.config:
+            dir = dir.without_file(".releaserc.json").with_new_file(
+                ".releaserc.json", json.dumps(self.config)
+            )
 
         return (
             dag.container()
